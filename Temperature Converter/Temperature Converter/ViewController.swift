e01354db8555eb3b691342e0e1133823c3db47a4
Kristians Tide <Ktide98@gmail.com>
2021-11-14 12:21:06 +0200

    made TempConventer fully functional. finished

--- Temperature Converter/Temperature Converter/ViewController.swift
+++ Temperature Converter/Temperature Converter/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tempConvLbl: UILabel!
+    @IBOutlet weak var celcNumSlider: UISlider!
+    @IBOutlet weak var celcNumLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-//test123 123
 
+    @IBAction func sliderToLbl(_ sender: Any) {
+        celcNumLbl.text = String(Int(round(celcNumSlider.value))) + "ºC"
+    }
+    
+    @IBAction func segmentControl(_ sender: UISegmentedControl) {
+        let celcTemp: Int = Int(celcNumSlider.value)
+        let kelvinTemp: Double = 273.15
+//        let FahrenTemp: Int = (celcTemp * (9 / 5)) + 32
+        if sender.selectedSegmentIndex == 0{
+            tempConvLbl.text = String(Float(kelvinTemp + Double(celcTemp))) + "ºK"
+        }else if sender.selectedSegmentIndex == 1{
+            tempConvLbl.text = String(Double(celcTemp * 9) / 5 + 32) + "ºF"
+        }
+    }
 }
 
